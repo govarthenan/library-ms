e01e4b704033319862992b2190dc5095dd118279
Govarthenan Rajadurai <govarthenan@gmail.com>
2023-10-06 13:44:48 +0530

    Add and list books

---
+++ Book.java
@@ -0,0 +1,47 @@
+public class Book {
+    int id;
+    String name, author, publisher;
+
+    // Constructor
+    public Book(int id, String name, String author, String publisher) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+}
--- Run.java
+++ Run.java
@@ -1,11 +1,24 @@
 
 // Main source file that aggregates all other classes and runs the whole program
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Run {
     // Input scanner for menu navigation
     static Scanner input = new Scanner(System.in);
     static String menuChoice; // Get the user's selection
+
+    // Database
+    static ArrayList<Book> books = new ArrayList<Book>();
+
+    // AutoID trackers
+    static int autoIdBook = 0;
+
+    public static void printArrayList(ArrayList<?> list) { // DEBUG
+        for (Object item : list) {
+            System.out.println(item);
+        }
+    }
 
     public static void printWelcome() {
         // Print the welcome message for the user
@@ -68,7 +81,64 @@ public class Run {
         }
     }
 
+    public static void addBook() {
+        String name, author, publisher; // Variables to store user input
+
+        System.out.println("\nAdd Book To the Shelf");
+
+        // Take input from user
+        System.out.print("Enter book name: ");
+        name = input.nextLine();
+
+        System.out.print("Enter author: ");
+        author = input.nextLine();
+
+        System.out.print("Enter publisher: ");
+        publisher = input.nextLine();
+
+        // Create new book and add it to the database
+        Book newBook = new Book(autoIdBook, name, author, publisher);
+        books.add(newBook);
+
+        autoIdBook++; // Increment auto ID
+    }
+
+    public static void listBooks(ArrayList<Book> books) {
+        // // Display a simple list of all the recorded books' full details
+        // System.out
+        // .println("\nBook ID" + "\t\t\t" + "Book Name" + "\t\t\t" + "Author Name" +
+        // "\t\t\t" + "Publisher Name");
+        // System.out.println("```````" + "\t\t\t" + "`````````" + "\t\t\t" +
+        // "```````````" + "\t\t\t" + "``````````````");
+        // // System.out.print("\n");
+
+        // for (Book book : books) {
+        // System.out.println(book.id + "\t\t\t" + book.name + "\t\t\t" + book.author +
+        // "\t\t\t" + book.publisher);
+        // }
+
+        // Define the format for each column
+        String format = "%-8s %-30s %-20s %-20s%n";
+
+        // Print the header row
+        System.out.format(format, "Book ID", "Book Name", "Author Name", "Publisher Name");
+        System.out.format(format, "-------", "---------", "-----------", "--------------");
+
+        // Print the book details
+        for (Book book : books) {
+            System.out.format(format, book.id, book.name, book.author, book.publisher);
+        }
+    }
+
     public static void main(String[] args) {
+        // Debug data
+        Book book1 = new Book(-3, "Harry Potter", "JK Rowling", "Indigo");
+        Book book2 = new Book(-2, "Percy Jackson", "Martin GR", "Pearson");
+        Book book3 = new Book(-1, "Lord of the rings", "KR Tolkien", "Sarasavi");
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+
         // Run the program in a conitnuous loop unless user wants to exit
         printWelcome();
 
@@ -79,16 +149,28 @@ public class Run {
             // validate the user's selection
             if (validateMenuChoice(menuChoice)) {
                 System.out.println(menuChoice + menuChoice.length());
+
                 if (menuChoice.equals("b")) {
-                    printBookMenu();
+                    printBookMenu(); // Print section menu
+                    menuChoice = input.nextLine(); // User selection of operation
+
+                    // See which operation the user input corresponds to and call it
+                    if (menuChoice.equals("a")) {
+                        addBook();
+                    } else if (menuChoice.equals("l")) {
+                        listBooks(books);
+                    }
+
                 } else if (menuChoice.equals("m")) {
                     printMemberMenu();
                 } else if (menuChoice.equals("l")) {
                     printLendingMenu();
                 }
+
             } else {
                 System.out.print("Menu choice too long. Enter one character only.\n\n\n");
             }
+
         }
     }
 }
