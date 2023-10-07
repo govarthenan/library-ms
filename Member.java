9cfab3aacb89c4fcf9477aa848d4c60d41839805
Govarthenan Rajadurai <govarthenan@gmail.com>
2023-10-07 18:54:07 +0530

    Add Member class

---
+++ Member.java
@@ -0,0 +1,37 @@
+public class Member {
+    private int id;
+    private String name, email;
+
+    // Constructor
+    public Member(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
